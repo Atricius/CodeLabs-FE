bd50a247e7636a27dbb1bfee20615b929888bdb2
Jason Coons <137571052+Atricius@users.noreply.github.com>
2023-09-13 20:32:48 -0500

    added notes

--- OtherExcerciseFiles/JavaScript/ClassExercises2023-09-11/Exercise01/scripts.js
+++ OtherExcerciseFiles/JavaScript/ClassExercises2023-09-11/Exercise01/scripts.js
@@ -5,3 +5,22 @@
 // 2. Print the value of this inside each function.
 // 3. Discuss or note down the differences you observe in the value of this across the three scenarios.
 
+function globalFunction() {
+     console.log(this); // this refers to the 'Window'
+}
+
+globalFunction();
+
+const object1 = {
+     name: "joe",
+     objectFunction: function() {
+          console.log(this); // this refers to the parent variable 'object1', including other values or properties of the object
+     },
+     arrowFunction: () => {
+          console.log(this); // this refers to scope of the parent object that the function is part of... in this case 'Global scope'
+     }
+};
+
+object1.objectFunction();
+object1.arrowFunction();
+
